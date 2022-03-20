d122e60992f9e0d9307d45eb2e82142e64ee7593
Your Name <you@example.com>
2022-03-19 23:10:11 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
+
+  const [nation, setNation] = useState (0);
+  const {id, country, population, image} = data[nation];
+
+  const previousNation = () => {
+    setNation ((nation=>{
+      nation--;
+      if (nation<0) {return data.length-1}
+      return nation
+    }))
+  }
+
+  const nextNation =() => {
+    setNation ((nation => {
+      nation++;
+      if (nation> data.length-1){nation=0}
+      return nation
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className='container'>
+      <h1> Countries in the world by population (2022)</h1>
+      </div>
+
+      <div className='container'>
+      <img src={image} alt="pic" width='300px'height='200px' />
+      </div>
+
+      <div className='container'>
+      <h2 className='country'> {id} - {country} </h2>
+      </div>
+
+      <div className='container'>
+      <h2> Population - {population} </h2>
+      </div>
+
+      <div className='container'>
+      <button onClick={previousNation}> Previous</button>
+      <button onClick={nextNation} >Next</button>
+      </div>
+
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,62 @@
+export const data = [
+    {
+        id: 1,
+        country: 'China',
+        population:'1 439 323 776',
+        image:"https://cdn.pixabay.com/photo/2014/05/18/19/13/beijing-347298__340.jpg"
+    },
+    {
+        id:2 ,
+        country:"India",
+        population:'1 380 004 385',
+        image:"https://cdn.pixabay.com/photo/2018/09/04/16/48/taj-mahal-3654227__340.jpg"
+    },
+    {
+        id:3 ,
+        country:"United States",
+        population: '331 002 651',
+        image:"https://cdn.pixabay.com/photo/2015/07/31/11/45/city-869064__340.jpg"
+    },
+    {
+        id:4 ,
+        country:"Indonesia",
+        population:'273 523 615',
+        image:'https://cdn.pixabay.com/photo/2019/06/13/16/06/dance-4271941__340.jpg'
+    },
+    {
+        id:5 ,
+        country:"Pakistan",
+        population:'220 892 340', 
+        image:'https://cdn.pixabay.com/photo/2020/03/31/11/05/crowd-4987223__340.jpg'
+    },
+    {
+        id:6 ,
+        country:"Brazil",
+        population:'212 559 417',
+        image:'https://cdn.pixabay.com/photo/2016/03/27/20/57/people-1284253__480.jpg'
+    },
+    {
+        id: 7,
+        country:"Nigeria",
+        population:'206 139 589',
+        image:'https://cdn.pixabay.com/photo/2016/08/08/08/48/nigeria-walk-1577895__340.jpg'
+    },
+    {
+        id:8 ,
+        country:"Bangladesh",
+        population:'164 689 383',
+        image:'https://cdn.pixabay.com/photo/2021/01/30/14/22/women-5963964__340.jpg'
+    },
+    {
+        id:9,
+        country:"Russia",
+        population:'145 934 462',
+        image:'https://cdn.pixabay.com/photo/2016/07/07/14/05/russia-1502468__340.jpg'
+    },
+    {
+        id:10,
+        country:"Mexico",
+        population:'128 932 753',
+        image:'https://cdn.pixabay.com/photo/2016/04/25/08/07/costa-maya-1351361__340.jpg'
+    }
+]
